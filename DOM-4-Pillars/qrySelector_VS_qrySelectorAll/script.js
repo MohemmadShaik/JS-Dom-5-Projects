5467d9f2107026408b7e3ec87ff70465406da9a4
MohemmadShaik <mohemmadshaik@gmail.com>
2024-10-08 21:58:18 +0530

    qrySelecetor VS qrySelector Diff completed.

---
+++ DOM-4-Pillars/qrySelector_VS_qrySelectorAll/script.js
@@ -0,0 +1,12 @@
+var a = document.querySelector("h1");
+var b = document.querySelector("h2");
+
+// a.innerHTML = ("Something text..");
+// console.log(a);
+a.textContent = "<h1>TEXT HERE<H1>";
+b.innerHTML = "<h1>TEXT HERE<H1>"
+
+// a.forEach(function(e){
+//     console.log(e);
+// })
+ 
